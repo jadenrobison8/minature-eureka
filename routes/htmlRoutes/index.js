dc26d3c145ee3e936bd54588d97cd3b1f5c04bec
robisonja <jadenrobison8@gmail.com>
2021-04-14 11:02:31 -0600

    html routes updates

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,20 @@
 const path = require('path');
+const router = require("express").Router();
+
+//route used to create a homepage for a server. communicates with html file
+router.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+//send notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+//send index.html
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 //exporting files
-module.exports = (app) => {
-    //send notes.html
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../public/notes.html'));
-    });
-
-    //send index.html
-    app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
-};
+module.exports = router;
