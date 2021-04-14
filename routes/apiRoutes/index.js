bbfe82de1d4eee6b2bc7c2a8947436ac8b3e300e
robisonja <jadenrobison8@gmail.com>
2021-04-13 20:25:19 -0600

    api routes completed

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 let notes = require("../../data/db.json");
 const fs = require("fs");
 const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
+//const writeFileAsync = util.promisify(fs.writeFile);
 
 //add notes route
 router.get("/notes", (req, res) => {
@@ -14,15 +14,21 @@ router.post("/notes", (req,res) => {
     let newNote = req.body;
 
     //check id
-    let id = notes[notes.length - 1]["id"];
-    let newId = id + 1;
-    newNote["id"] = newId;
+    let id = notes.length;
+    newNote["id"] = id;
 
     console.log("body: ", req.body);
     notes.push(newNote);
 
-    writeFileAsync("../../data/db.json", JSON.stringify(notes));
+    fs.writeFileSync("./data/db.json", JSON.stringify(notes), (err) => {
+        if (err) throw err;
+    });
     console.log("notes have been updated!");
+
+    res.json(newNote);
 });
+
+//delete a note
+
 
 module.exports = router;
