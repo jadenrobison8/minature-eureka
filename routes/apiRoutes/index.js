7251fb8773c91b9be656e58150697eb057e59bbd
robisonja <jadenrobison8@gmail.com>
2021-04-13 20:01:32 -0600

    get route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,28 @@
-const fs = require('fs');
+const router = require("express").Router();
+let notes = require("../../data/db.json");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-//GET notes
+//add notes route
+router.get("/notes", (req, res) => {
+    res.json(notes);
+});
 
-//POST notes
+//POST route
+router.post("/notes", (req,res) => {
+    let newNote = req.body;
 
-//DELETE notes
+    //check id
+    let id = notes[notes.length - 1]["id"];
+    let newId = id + 1;
+    newNote["id"] = newId;
+
+    console.log("body: ", req.body);
+    notes.push(newNote);
+
+    writeFileAsync("../../data/db.json", JSON.stringify(notes));
+    console.log("notes have been updated!");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const { notes } = require('./data/db.json');
 
 const app = express();
-
 const PORT = process.env.port || 3001;
-
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./routes/apiRoutes")(app);
-require('./routes/htmlRoutes')(app);
+//use routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server  now on port: ${PORT}`);
