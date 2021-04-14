e51c71ca0e3cd1b299c0032d232a6f5154ae64b3
robisonja <jadenrobison8@gmail.com>
2021-04-14 10:32:16 -0600

    edited heroku build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const app = express();
 const PORT = process.env.port || 3001;
+const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
